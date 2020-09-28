79ac6fc7401896ed01be30458e055635ada710b0
Arthur91S <arturs.stulpe@gmail.com>
2020-09-28 14:49:58 +0100

    Clean up sql query

--- create_bookish_db.sql
+++ create_bookish_db.sql
@@ -4,79 +4,43 @@ USE bookish2;
 
 CREATE TABLE books (
 	id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(100) NOT NULL,
-    isbn VARCHAR(100) NULL,
-    copies_available INT NULL,
+    title VARCHAR(255) NOT NULL,
+    isbn VARCHAR(13) NULL UNIQUE,
+    copies_total INT NULL,
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE book_authors (
+CREATE TABLE authors (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE author_books (
     id INT NOT NULL AUTO_INCREMENT,
     book_id INT NOT NULL,
     author_id INT NOT NULL,
      FOREIGN KEY (book_id) REFERENCES books(id),
-     FOREIGN KEY (author_id) REFERENCES book_authors(id),
+     FOREIGN KEY (author_id) REFERENCES authors(id),
      PRIMARY KEY(id)
 );
 
 CREATE TABLE members (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE books_lent (
+CREATE TABLE checked_out_books (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     book_id INT NOT NULL,
-    checked_out_on DATETIME NOT NULL,
-    due_back_on DATETIME NOT NULL,
+    checked_out_on DATE NOT NULL,
+    due_back_on DATE NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (user_id) REFERENCES members(id),
     FOREIGN KEY (book_id) REFERENCES books(id)
 );
-
-
-
-------------------------------------------------------
-
-
-CREATE SCHEMA `bookish`;
-
-CREATE TABLE `bookish`.`books` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(100) NOT NULL,
-    `isbn` VARCHAR(100) NULL,
-    `copies_available` INT NULL,
-	PRIMARY KEY(`id`)
-);
-
-CREATE TABLE `bookish`.`book_authors` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `book_id` INT NOT NULL,
-    `author_id` INT NOT NULL,
-     FOREIGN KEY (`book_id`) REFERENCES `books`(`id`),
-     FOREIGN KEY (`author_id`) REFERENCES `book_authors`(`id`)
-);
-
-CREATE TABLE `bookish`.`members` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NOT NULL,
-    PRIMARY KEY(`id`)
-);
-
-CREATE TABLE `bookish`.`books_lent` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
-    `book_id` INT NOT NULL,
-    `checked_out_on` DATETIME NOT NULL,
-    `due_back_on` DATETIME NOT NULL,
-    PRIMARY KEY(`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `members`(`id`),
-    FOREIGN KEY (`book_id`) REFERENCES `books`(`id`)
-);
-
-
 
 
 
